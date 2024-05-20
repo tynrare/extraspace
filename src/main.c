854f2cdcbafdb09f1ddd3bcdc2a7dc2e1f27ee0f
tynroar <tynrare@gmail.com>
2024-05-20 22:45:51 +0300

    cube-lights-added

--- src/main.c
+++ src/main.c
@@ -2,7 +2,11 @@
 
 #include "root.h"
 #include <raylib.h>
+#include <raymath.h>
 #include <stdio.h>
+
+#define RLIGHTS_IMPLEMENTATION
+#include "external/rlights.h"
 
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -16,18 +20,55 @@ static const int VIEWPORT_H = 0x1c2;
 static int viewport_w = VIEWPORT_W;
 static int viewport_h = VIEWPORT_H;
 
+// --- app
+Camera camera = { 0 };
+Model model = { 0 };
+Shader shader = { 0 };
+
 static void draw() {
   ClearBackground(BLACK);
+
+	SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], &camera.position.x, SHADER_UNIFORM_VEC3);
+
+	BeginMode3D(camera);
+		DrawModel(model, (Vector3){ 0, 0, 0}, 1.0f, WHITE);
+	EndMode3D();
 
   DrawText("tynbox", 16, 16, 20, WHITE);
   DrawText("tynbox", viewport_w - 16 - 72, viewport_h - 16 - 20, 20, WHITE);
 }
 
-static void dispose() {}
-
-static void init() { dispose(); }
-
 static void inputs() {}
+
+// --- flow
+
+static void dispose() {
+	UnloadModel(model);
+	UnloadShader(shader);
+}
+
+static void init() { 
+	dispose(); 
+
+	shader = LoadShader(RES_PATH "lighting.vs", RES_PATH "lighting.fs");
+	// Get some required shader locations
+	shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
+	int ambientLoc = GetShaderLocation(shader, "ambient");
+	SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
+	CreateLight(LIGHT_DIRECTIONAL, (Vector3){ -2, 1, -2 }, Vector3Zero(), WHITE, shader);
+
+	model = LoadModelFromMesh(GenMeshCube(1, 2, 1));
+	model.materials[0].shader = shader;
+
+	camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
+
+}
+
+// --- system
 
 static long resize_timestamp = -1;
 static const float resize_threshold = 0.3;
