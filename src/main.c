6cfaea42df58564a6c9b923ad97ddb0c94de2714
tynroar <tynrare@gmail.com>
2024-05-31 03:27:55 +0300

    correct fly controls

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <stdio.h>
+//#include <GL/gl.h>
+#include "rlgl.h"
 
 #define RLIGHTS_IMPLEMENTATION
 #include "external/rlights.h"
@@ -20,14 +22,95 @@ static const int VIEWPORT_H = 0x1c2;
 static int viewport_w = VIEWPORT_W;
 static int viewport_h = VIEWPORT_H;
 
+// ---
+
+Vector3 Vector3RotateByMatrix(Vector3 v, Matrix m) {
+	v = Vector3Transform(v, m);
+
+	// extract position
+	Vector3 pos = Vector3Transform(Vector3Zero(), m);
+	v = Vector3Subtract(v, pos);
+
+	return v;
+}
+
+Matrix MatrixJustRotate(Vector3 r, Matrix m) {
+	Vector3 pos = Vector3Transform(Vector3Zero(), m);
+	// 1. discard origin
+	m = MatrixMultiply(m, MatrixTranslate(-pos.x, -pos.y, -pos.z));
+	// 2. rotate
+	m = MatrixMultiply(m, MatrixRotateXYZ(r));
+	// 3. translate back
+	m = MatrixMultiply(m, MatrixTranslate(pos.x, pos.y, pos.z));
+
+	return m;
+}
+
+// ---
+static void update_pawn();
+
+// ---
+
+bool _draw_help_enabled = false;
+bool _pawn_acceleration_input_enabled = false;
+bool _pawn_torque_input_enabled = false;
+bool _pawn_torque_input_alt_axis_enabled = false;
+
 // --- app
-Camera camera = { 0 };
+Camera3D camera = { 0 };
 Model model = { 0 };
+Model model_sphere = { 0 };
 Shader shader = { 0 };
 Vector3 torque = { 0 };
+Vector3 velocity = { 0 };
+Texture texture = { 0 };
 
 static void update() {
+	Vector3 cpos = { 0, 0.0f, 10.0f };
+	Vector3 up = { 0, 0.1f, 0.0f };
+	bool attach_camera = true;
+	if (attach_camera) {
+		up = Vector3RotateByMatrix((Vector3){ 0, 1.0f, 0.0f }, model.transform);
+		camera.position = Vector3Transform(cpos, model.transform);
+	} else {
+		camera.position = cpos;
+	}
+
+	camera.target = Vector3Transform(Vector3Zero(), model.transform);
+	camera.up = up;
+
+	update_pawn();
 }
+
+static void draw_dbg_3d() {
+	Vector3 ra = Vector3Transform((Vector3){ 1.0f, 0.0f, 0.0f }, model.transform);
+	Vector3 rb = Vector3Transform(Vector3Zero(), model.transform);
+	Vector3 ga = Vector3Transform((Vector3){ 0.0f, 1.0f, 0.0f }, model.transform);
+	Vector3 gb = Vector3Transform(Vector3Zero(), model.transform);
+	Vector3 ba = Vector3Transform((Vector3){ 0.0f, 0.0f, 2.0f }, model.transform);
+	Vector3 bb = Vector3Transform(Vector3Zero(), model.transform);
+	DrawLine3D(ra, rb, RED);
+	DrawLine3D(ga, gb, GREEN);
+	DrawLine3D(ba, bb, BLUE);
+}
+
+static void draw_help() {
+	if (_draw_help_enabled) {
+		DrawRectangleRec((Rectangle){16, 16, 420, 256}, Fade(BLACK, 0.3));
+		DrawText("LMB: Accelerate.", 32, 32 + 22 * 0, 20, WHITE);
+		DrawText("RMB: Change rotation axis.", 32, 32 + 22 * 1, 20, WHITE);
+		DrawText("Space: toggle rotation.", 32, 32 + 22 * 2, 20, WHITE);
+	} else {
+		Color color = WHITE;
+		if (_pawn_torque_input_enabled) {
+			color = RED;
+		}
+
+		DrawText("SPACE.", 16, 16, 20, color);
+		DrawText("'H': toggle help.", viewport_w - 16 - 170, viewport_h - 16 - 20, 20, WHITE);
+	}
+}
+
 
 static void draw() {
   ClearBackground(BLACK);
@@ -36,40 +119,100 @@ static void draw() {
 
 	BeginMode3D(camera);
 		DrawModel(model, (Vector3){ 0, 0, 0}, 1.0f, WHITE);
+
+		draw_dbg_3d();
+
+		rlDisableBackfaceCulling();
+		DrawModel(model_sphere, (Vector3){ 0, 0, 0}, 3.3f, WHITE);
+		rlEnableBackfaceCulling();
 	EndMode3D();
 
-	Color color = WHITE;
-	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-		color = RED;
+	draw_help();
+}
+
+static void update_pawn() {
+	// rotation
+	model.transform = MatrixJustRotate(torque, model.transform);
+
+	// acceleration
+	Matrix mvel = MatrixTranslate(velocity.x, velocity.y, velocity.z);
+	model.transform = MatrixMultiply(model.transform, mvel);
+
+	// dumping down movement (not very spacish)
+	torque = Vector3Lerp(torque, Vector3Zero(), 1e-2);
+	velocity = Vector3Lerp(velocity, Vector3Zero(), 1e-2);
+}
+
+static void inputs_pawn_torque() {
+	if (!_pawn_torque_input_enabled) {
+		return;
 	}
-  DrawText("Left Mouse Button.", 16, 16, 20, color);
-  DrawText("tynbox", viewport_w - 16 - 72, viewport_h - 16 - 20, 20, color);
+
+	Vector3 ltorque = { 0 };
+	float factor = 1e-4;
+
+	Vector2 ppos = GetMouseDelta();
+	ltorque.y += ppos.x * factor;
+	ltorque.x += ppos.y * factor;
+
+	if (_pawn_torque_input_alt_axis_enabled) {
+		ltorque.z = ltorque.y;
+		ltorque.y = 0;
+		// rotating on both xz axis wobbly. disabling it here
+		ltorque.x = 0;
+	}
+
+  // do not apply this transform if you want to rotate in global coorditanes:
+	ltorque = Vector3RotateByMatrix(ltorque, model.transform);
+
+	torque = Vector3Add(torque, ltorque);
+}
+
+static void inputs_pawn_acceleration() {
+	if (!_pawn_acceleration_input_enabled) {
+		return;
+	}
+	
+	float factor = 1e-2;
+	Vector3 acceleration = { 0.0f, 0.0f, -factor };
+
+	acceleration = Vector3RotateByMatrix(acceleration, model.transform);
+
+	velocity = Vector3Add(velocity, acceleration);
+}
+
+static void inputs_pawn() {
+	inputs_pawn_torque();
+	inputs_pawn_acceleration();
 }
 
 static void inputs() {
-	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-		float factor = 1e-4;
-		Vector2 ppos = GetMouseDelta();
-		// conversion from 2d xz flipped
-		torque.y += ppos.x * factor;
-		torque.x += ppos.y * factor;
+	if (IsKeyPressed(KEY_H)) {
+		_draw_help_enabled = !_draw_help_enabled;
 	}
 
-  // do not apply this transform if you want to rotate in gloval coorditanes
-	//Vector3 t =  Vector3Transform(torque, model.transform);
-	Vector3 t = torque;
+	if (IsKeyPressed(KEY_SPACE)) {
+		_pawn_torque_input_enabled = !_pawn_torque_input_enabled;
+		if (_pawn_torque_input_enabled) {
+			DisableCursor();
+		} else {
+			EnableCursor();
+		}
+	}
 
-	Matrix mx = MatrixRotateX(t.x);
-	Matrix my = MatrixRotateY(t.y);
-	Matrix m1 = MatrixMultiply(mx, my);
-	model.transform = MatrixMultiply(model.transform, m1);
+	_pawn_acceleration_input_enabled = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+	_pawn_torque_input_alt_axis_enabled = IsMouseButtonDown(MOUSE_BUTTON_RIGHT);
+
+	inputs_pawn();
 }
 
 // --- flow
 
 static void dispose() {
 	UnloadModel(model);
+	UnloadModel(model_sphere);
 	UnloadShader(shader);
+	UnloadTexture(texture);
 }
 
 static void init() { 
@@ -82,8 +225,13 @@ static void init() {
 	SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
 	CreateLight(LIGHT_DIRECTIONAL, (Vector3){ -2, 1, -2 }, Vector3Zero(), WHITE, shader);
 
+	texture = LoadTexture(RES_PATH "tex1.png");
+
 	model = LoadModelFromMesh(GenMeshCube(1, 1, 2));
 	model.materials[0].shader = shader;
+	model_sphere = LoadModelFromMesh(GenMeshSphere(10, 16, 16));
+	//model_sphere.materials[0].shader = shader;
+	model_sphere.materials[0].maps->texture = texture;
 
 	camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
 	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
